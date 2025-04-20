61fc108ec3b3128950a68a52061548564d36fb29
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-20 00:47:48 -0300

    Ejercicio de reloj finalizado

--- js/date.js
+++ js/date.js
@@ -1,226 +1,188 @@
-const hourHTML = document.getElementById("hora");
-const minuteHTML = document.getElementById("minuto");
-const secondHTML = document.getElementById("segundo");
-const dateHTML = document.getElementById("text");
+const hourHTML = document.getElementById("hour");
+const minutesHTML = document.getElementById("minutes");
+const secondsHTML = document.getElementById("seconds");
+const dateHTML = document.getElementById("dateText");
 
-const fechaInicial = new Date();
+console.log(hourHTML);
+console.log(minutesHTML);
+console.log(secondsHTML);
 
-// Método para formatear fecha en español usando la API Intl -> Internationalization
-const formateador = new Intl.DateTimeFormat("es-AR", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-})
+// hourHTML.innerText = "18";
+// minutesHTML.innerText = "38";
+// secondsHTML.innerText = "03";
 
-console.log(formateador.format(fechaInicial))
+const date = new Date();
 
-pintarReloj(fechaInicial)
-pintarTextoFecha(fechaInicial)
+// console.log(fecha);
 
+let hour = date.getHours();
 
-setInterval(function () {
-  const fecha = new Date();
-
-  pintarReloj(fecha)
-  pintarTextoFecha(fecha)
-  
-}, 1000);
-
-function pintarTextoFecha(fecha) {
-  const year = fecha.getFullYear();
-
-  const day = fecha.getDate(); // 1-31
-
-  const month = fecha.getMonth(); // 2 = marzo  0-11
-
-  const monthName = obtenerNombreMes( month ); // "Diciembre"
-
-  const dayOfWeek = fecha.getDay(); // 0-6 (0 = domingo, 1 = lunes, ...)
-
-  const dayName = obtenerNombreDia(dayOfWeek) // Funcion que llamandola y pasando el dayOfWeek devuelve el nombre del dia de la semana
-
-  // "HOY ES LUNES 17 DE MARZO DE 2025"
-  const text = `HOY ES ${dayName} ${day} DE ${monthName} DE ${year}`;
-
-  dateHTML.innerText = text;
+if(hour < 10) {
+    hour = "0" + hour;
 }
 
-function pintarReloj(fecha) {
-    let hora = fecha.getHours();
+hourHTML.innerText = hour;
 
-    if (hora < 10) {
-      // Deberíamos agregar un 0 por delante al valor de la hora
-      hora = "0" + hora; // "02"
+let minutes = date.getMinutes();
+
+if(minutes < 10) {
+    minutes = "0" + minutes;
+}
+
+
+minutesHTML.innerText = minutes;
+
+let seconds = date.getSeconds();
+
+if(seconds < 10) {
+    seconds = "0" + seconds;
+}
+
+secondsHTML.innerText = seconds;
+
+// console.log("Comenzando Timeout");
+
+// setTimeout(function(){
+//     console.log("Función de Timeout ejecutado");
+// }, 2000) 
+
+// setTimeout no nos funcionaría porque no se vuelve a ejecutar, solo se ejecuta una vez.
+
+// console.log("Comenzando Intervalo");
+
+// setInterval(function(){
+//     console.log("Intervalo ejecutado");
+// }, 1000)
+
+setInterval(function(){
+    const date = new Date();
+
+    let hour = date.getHours();
+
+    if(hour < 10) {
+        hour = "0" + hour;
     }
 
-    // Insertar la hora en el HTML
-    hourHTML.innerText = hora;
+    hourHTML.innerText = hour;
 
-    let minute = fecha.getMinutes();
+    let minutes = date.getMinutes();
 
-    if (minute < 10) {
-      minute = "0" + minute;
+    if(minutes < 10) {
+        minutes = "0" + minutes;
     }
 
-    // Insertar los minutos en el HTML
-    minuteHTML.innerText = minute;
+    minutesHTML.innerText = minutes;
 
-    let second = fecha.getSeconds();
+    let seconds = date.getSeconds();
 
-    if (second < 10) {
-      second = "0" + second;
+    if(seconds < 10) {
+        seconds = "0" + seconds;
     }
 
-    // Insertar los segundos en el HTML
-    secondHTML.innerText = second;
+    secondsHTML.innerText = seconds;
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+
+    const day = date.getDate();
+
+    const monthName = getMonthName(month);
+
+    const weekDay = date.getDay();
+
+    const dayName = getDayName(weekDay);
+
+    const text = `Hoy es ${dayName}, ${day} de ${monthName} de ${year}`;
+
+    dateHTML.innerText = text;
+
+}, 1000)
+
+function getMonthName(month) {
+
+    const months = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ];
+    // const monthName = months[month];
+    return months[month];
+
+    // if(month === 0) {
+    //     return "Enero";
+    // } else if(month === 1) {
+    //     return "Febrero";
+    // } else if(month === 2) {
+    //     return "Marzo";
+    // } else if(month === 3) {
+    //     return "Abril";
+    // } else if(month === 4) {
+    //     return "Mayo";
+    // } else if(month === 5) {
+    //     return "Junio";
+    // } else if(month === 6) {
+    //     return "Julio";
+    // } else if(month === 7) {
+    //     return "Agosto";
+    // } else if(month === 8) {
+    //     return "Septiembre";
+    // } else if(month === 9) {
+    //     return "Octubre";
+    // } else if(month === 10) {
+    //     return "Noviembre";
+    // } else if(month === 11) {
+    //     return "Diciembre";
+    // }
+
+    // switch (month) {
+    //     case 0:
+    //         return "Enero";
+    //     case 1:
+    //         return "Febrero";
+    //     case 2:
+    //         return "Marzo";
+    //     case 3:
+    //         return "Abril";
+    //     case 4:
+    //         return "Mayo";
+    //     case 5:
+    //         return "Junio";
+    //     case 6:
+    //         return "Julio";
+    //     case 7:
+    //         return "Agosto";
+    //     case 8:
+    //         return "Septiembre";
+    //     case 9:
+    //         return "Octubre";
+    //     case 10:
+    //         return "Noviembre";
+    //     case 11:
+    //         return "Diciembre";
+    //     default:
+    //         return null;
+    // }
 }
 
+function getDayName(weekDay) {
+    const days = [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado"
+    ];
 
-function obtenerNombreDia(day) {
-  const daysOfWeek = [
-    "Domingo",
-    "Lunes",
-    "Martes",
-    "Miércoles",
-    "Jueves",
-    "Viernes",
-    "Sábado"
-  ]
-
-  return daysOfWeek[day]
+    return days[weekDay];
 }
-
-
-function obtenerNombreMes(mes) {
-
-  const meses = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre"
-  ]
-
-  return meses[mes];
-  // const nombreMes = meses[mes]
-
-
-  // switch(mes) {
-  //   case 0:
-  //     return "Enero";
-  //   case 1:
-  //     return "Febrero";
-  //   case 2:
-  //     return "Marzo";
-  //   case 3:
-  //     return "Abril";
-  //   case 4:
-  //     return "Mayo";
-  //   case 5:
-  //     return "Junio";
-  //   case 6:
-  //     return "Julio";
-  //   case 7:
-  //     return "Agosto";
-  //   case 8:
-  //     return "Septiembre";
-  //   case 9:
-  //     return "Octubre";
-  //   case 10:
-  //     return "Noviembre";
-  //   case 11:
-  //     return "Diciembre";
-  //   default:
-  //     return null;
-  // }
-
-  // if(mes === 0) return "Enero";
-
-  // if(mes === 1) return "Febrero";
-
-  // if(mes === 2) return "Marzo";
-
-  // if(mes === 3) return "Abril";
-
-  // if(mes === 4) return "Mayo";
-
-  // if(mes === 5) return "Junio";
-
-  // if(mes === 6) return "Julio";
-
-  // if(mes === 7) return "Agosto";
-
-  // if(mes === 8) return "Septiembre";
-
-  // if(mes === 9) return "Octubre";
-
-  // if(mes === 10) return "Noviembre";
-
-  // if(mes === 11) return "Diciembre";
-
-  // if (mes === 0) {
-  //   return "Enero";
-
-  // } else if (mes === 1) {
-
-  //   return "Febrero";
-
-  // } else if (mes === 2) {
-
-  //   return "Marzo";
-
-  // } else if (mes === 3) {
-
-  //   return "Abril";
-
-  // } else if (mes === 4) {
-
-  //   return "Mayo";
-
-  // } else if (mes === 5) {
-
-  //   return "Junio";
-
-  // } else if (mes === 6) {
-
-  //   return "Julio";
-
-  // } else if (mes === 7) {
-
-  //   return "Agosto";
-
-  // } else if (mes === 8) {
-
-  //   return "Septiembre";
-
-  // } else if (mes === 9) {
-
-  //   return "Octubre";
-
-  // } else if (mes === 10) {
-
-  //   return "Noviembre";
-
-  // } else if (mes === 11) {
-
-  //   return "Diciembre";
-  // }
-
-  console.log("Fin de la función obtenerNombreMes")
-
-}
-
-// Ejecutar una función después de un tiempo determinado
-// setTimeout(funcion, tiempo en milisegundos)
-setTimeout(function () {
-  console.log("Función de Timeout ejecutado");
-}, 5000);
-
-// clearInterval(intervalo); // Detener el intervalo
